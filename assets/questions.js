e6e6ef39e3588c9b92eecc79310b420d4a9fdd67
Celyph <briannaslicer@gmail.com>
2023-05-04 09:58:34 -0500

    working on question counter popping up

---
+++ assets/questions.js
@@ -0,0 +1,62 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Which Pokemon is referred to as the Wish Pokemon?",
+        answer: "Jirachi",
+        options: [
+            "Whiscash",
+            "Jirachi",
+            "Staravia",
+            "Shinx"
+        ]
+    },
+
+{
+    numb: 2,
+    question: "How many pure Rock type Pokemon are there?",
+    answer: "9",
+    options: [
+        "3",
+        "8",
+        "14",
+        "9"
+    ]
+},
+
+{
+    numb: 3,
+    question: "Which of the following Pokemon are NOT starter Pokemon?",
+    answer: "Meowth",
+    options: [
+        "Meowth",
+        "Cyndaquil",
+        "Oshawott",
+        "Snivy"
+    ]
+},
+
+{
+    numb: 4,
+    question: "Which of the following were the first Pokemon games to come out?",
+    answer: "Green and Red",
+    options: [
+        "Scarlet and Violet",
+        "Green and Red",
+        "Sun and Moon",
+        "Black and White"
+    ]
+},
+
+{
+    numb: 5,
+    question: "What is the most common Pokemon type?",
+    answer: "Water",
+    options: [
+        "Normal",
+        "Fire",
+        "Dark",
+        "Water"
+    ]
+},
+
+];
--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,82 @@
-localStorage.setItem("fido", "pretend this is meaningful text")
-console.log(localStorage.getItem("fido"))
+localStorage.setItem("fido", "pretend this is meaningful text");
+console.log(localStorage.getItem("fido"));
 
 for (let i = 0; i < 5; i++) {
-    console.log(localStorage.getItem("fido"))  
+  console.log(localStorage.getItem("fido"));
 }
 
 function StartBox() {
-    const start_box = document.getElementById("start_box");
-    start_box.classList.add("activeInfo");
-    start_box.style.opacity = 1;
-       document.querySelector(".start_btn").style = "display: none"; 
-       document.querySelector(".info").style = "display: block"; 
-       document.querySelector(".continue_btn").style = "display: block";
-       document.querySelector(".restart_btn").style = "display: block";
+  const start_box = document.getElementById("start_box");
+  start_box.classList.add("activeInfo");
+  start_box.style.opacity = 1;
+  document.querySelector(".start_btn").style = "display: none";
+  document.querySelector(".info").style = "display: block";
+  document.querySelector(".continue_btn").style = "display: block";
+  document.querySelector(".restart_btn").style = "display: block";
 }
 
-function Continue(){
-    const start_box = document.getElementById("start_box");
-       document.querySelector(".start_btn").style = "display: none"; 
-       document.querySelector(".start_box").style = "display: none"; 
-       document.querySelector(".next_btn").style = "display: block";
+function Continue() {
+  const start_box = document.getElementById("start_box");
+  document.querySelector(".start_btn").style = "display: none";
+  document.querySelector(".start_box").style = "display: none";
+  document.querySelector(".next_btn").style = "display: block";
 }
 
-function QuizBox(){
-    const quiz_box = document.getElementById("quiz_box");
-       document.querySelector(".quiz_box").style = "display: block"; 
-
-       quiz_box.style.opacity = 1;
+function QuizBox() {
+  const quiz_box = document.getElementById("quiz_box");
+  document.querySelector(".quiz_box").style = "display: block";
+  quiz_box.style.opacity = 1;
 }
 
-function HideBox(){
-   const start_box = document.getElementById("start_box");
-    start_box.style.opacity = 0;
-       document.querySelector(".start_btn").style = "display: flex"; 
-       document.querySelector(".start_box").style = "display: block"; 
-       document.querySelector(".quiz_box").style = "display: none";
-       document.querySelector(".next_btn").style = "display: none";
+function HideBox() {
+  const start_box = document.getElementById("start_box");
+  start_box.style.opacity = 0;
+  document.querySelector(".start_btn").style = "display: flex";
+  document.querySelector(".start_box").style = "display: block";
+  document.querySelector(".quiz_box").style = "display: none";
+  document.querySelector(".next_btn").style = "display: none";
+}
+
+let que_count = 0;
+let que_numb = 1;
+
+const next_btn = quiz_box.querySelector(".next_btn");
+
+function nextQuestion() {
+  if (que_count < questions.length - 1) {
+    que_count++;
+    que_numb++;
+    showQuestions(que_count);
+    queCounter(que_numb);
+  } else {
+    console.log("Quiz finished!");
+  }
+}
+
+function showQuestions(index) {
+  const que_text = document.querySelector(".que_text");
+  const answer_list = document.querySelector(".answer_list");
+  let que_tag ="<span>" + questions[index].numb + ". " + questions[index].question + "</span>";
+  let answer_tag = '<div class="answer">' + questions[index].options[0] + "<span></span></div>" 
+                + '<div class="answer">' + questions[index].options[1] + "<span></span></div>" 
+                + '<div class="answer">' + questions[index].options[2] + "<span></span></div>"
+                + '<div class="answer">' + questions[index].options[3] + "<span></span></div>";
+  que_text.innerHTML = que_tag;
+  answer_list.innerHTML = answer_tag;
+  const answer = answer_list.querySelectorAll(".answer");
+  for (let index = 0; index < answer.length; i++) {
+    answer[i].setAttribute("onclick", "answerSelected(this");
+  }
+}
+
+function optionsSelected(answer) {
+  let userAns = answer.textContent;
+  let correctAns = questions[que_count].answer;
+  console.log(userAns);
+}
+
+function queCounter(index) {
+  const bottom_ques_counter = quiz_box.querySelector(".total_que");
+  let totalQuestionCountTag = "<span><p>" + que_count + "</p>of<p>" + questions.length + "</p>Questions</span>";
+  bottom_ques_counter.innerHTML = totalQuestionCountTag;
 }
