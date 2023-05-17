3769fd13c8f748b9633a54dcb4afb449cd01474b
Celyph <briannaslicer@gmail.com>
2023-05-17 13:04:27 -0500

    Working on adding more button functionality on the quiz options

--- assets/script.js
+++ assets/script.js
@@ -8,14 +8,17 @@ function StartBox() {
   document.querySelector(".start_btn").style = "display: none";
   document.querySelector(".info").style = "display: block";
   document.querySelector(".continue_btn").style = "display: block";
-  document.querySelector(".restart_btn").style = "display: block";
+  document.querySelector(".restart_btn").style = "display: none";
 }
 
 function Continue() {
   const start_box = document.getElementById("start_box");
   document.querySelector(".start_btn").style = "display: none";
   document.querySelector(".start_box").style = "display: none";
+  document.querySelector(".continue_btn").style = "display: none";
+  document.querySelector(".restart_btn").style = "display: none";
   document.querySelector(".next_btn").style = "display: block";
+
   let timer = setInterval(myTimer, 1000);
   function myTimer() {
     const d = document.querySelector(".timer_time");
@@ -58,13 +61,7 @@ function nextQuestion() {
 }
 
 const answer_list = document.querySelector(".answer_list");
- answer_list.addEventListener("click", function(event) {
-  const element = event.target;
-  console.log("kachow")
-  if (element.matches(".answer")) {
-    console.log("button worked")
-  }
-}); 
+
 
 function showQuestions(index) {
   const que_text = document.querySelector(".que_text");
@@ -74,33 +71,26 @@ function showQuestions(index) {
                 + '<button class="answer ans2">' + questions[index].options[1] + "<span></span></button>" 
                 + '<button class="answer ans3">' + questions[index].options[2] + "<span></span></button>"
                 + '<button class="answer ans4">' + questions[index].options[3] + "<span></span></button>";
-              //  let answerBtns = document.querySelectorAll(".answer")
-              //  for (let i = 0; i < answerBtns.length; i++) {
-              //   answerBtns[i].addEventListener("click", console.log(this))
-                
-              //  }
   que_text.innerHTML = que_tag;
   answer_list.innerHTML = answer_tag;
   const answer = document.querySelectorAll(".answer");
-  // for (let index = 0; index < answer.length; index++) {
-  //   answer[index].setAttribute("onclick", "answerSelected(this)");
-  // }
+  for (let i = 0; i < answer.length; i++) {
+    answer[i].setAttribute("onclick", "answerSelected(this)");
+  }
 }
 
 function answerSelected(answer) {
   let userAns = answer.textContent;
   let correctAns = questions[que_count].answer;
-  console.log(answer);
 
   if(userAns == correctAns){
     answer.classList.add("correct")
     console.log("correct")
-
   }
-    
   else{
     time-=2
     answer.classList.add("wrong")
+    console.log("wrong")
   }
 }
 
